079f8e12b635a5b9970aa19291d03c5b10f0f9fa
Meeta Haldar <69325431+MeetaHaldar@users.noreply.github.com>
2021-04-08 12:10:32 +0530

    Add files via upload

---
+++ 01_array_dec.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     int a[] = {2, 4, 5, 8, 8, 9};
+//     for (int x : a)
+//     {
+//         cout << x << endl;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     float a[] = {2.8, 4, 5.7, 8, 8.1, 9};
+//     for (int x : a)
+//     {
+//         cout << x << endl;
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     float a[] = {2.8, 4, 5.7, 8, 8.1, 9};
+//     for (float x : a)
+//     {
+//         cout << x << endl;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     char a[] = {'A', 66, 67, 68, 'e'};
+//     for (int x : a)
+//     {
+//         cout << x << endl;
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     char a[] = {'A', 66, 67, 68, 'e'};
+//     for (char x : a)
+//     {
+//         cout << x << endl;
+//     }
+//     return 0;
+// }
+int main()
+{
+    char a[] = {'A', 66, 67, 68, 'e'};
+    for (auto x : a)
+    {
+        cout << x << endl;
+    }
+    return 0;
+}
