723f6faf378f8dd41c7b2cf6f23c9363bf4e1601
Meeta Haldar <69325431+MeetaHaldar@users.noreply.github.com>
2021-04-15 21:46:52 +0530

    Add files via upload

---
+++ 03_size_of_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the total number of element = ";
+    cin >> n;
+    int a[n];
+    cout << "the size of your array is = " << sizeof a;
+    return 0;
+}
