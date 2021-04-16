8070af89ce8d468161c2d68ce11481398dcf09d0
Meeta Haldar <69325431+MeetaHaldar@users.noreply.github.com>
2021-04-17 00:31:08 +0530

    Add files via upload

---
+++ 08_call_By_address.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    cout << " the value of x and y in the function is" << endl
+         << "*x = " << *x << ", *y = " << *y << endl;
+
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    cout << " the value of formal parameters after the swap is " << endl
+         << "*x = " << *x << " , *y= " << *y << endl;
+}
+
+int main()
+{
+    int a = 3, b = 6;
+    cout << "before applying  the function" << endl
+         << "a = " << a
+         << " , b = " << b << endl;
+
+    swap(&a, &b);
+
+    cout << "after applying the function" << endl
+         << "a= " << a << ", b = " << b << endl;
+    cout << "Hence, Now there will be  change in actual parameter becuase of pointer .";
+    return 0;
+}
