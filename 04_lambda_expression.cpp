a8d9f0aaa81025ecca5f93699cda82497d9447b2
Meeta Haldar <69325431+MeetaHaldar@users.noreply.github.com>
2021-05-18 11:42:31 +0530

    one line function

---
+++ 04_lambda_expression.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // simple one line function.
+    // [captured-list](parameters)->return type {outpt}(function call)
+    
+
+    []() { cout << "hii its me meeta haldar ."; }();
+}
